5d3e855ab38b445c13533a912ba6208b00231113
nakamigawa <nak@nakme.net>
2024-04-09 08:46:27 +0900

    Since it is a SPA, make it delayed execution

--- src/content.js
+++ src/content.js
@@ -1,12 +1,14 @@
 function listUpAndExecute() {
-    Array.from(document.getElementsByTagName('relative-time')).forEach(apply);
-    Array.from(document.getElementsByTagName('time-ago')).forEach(apply);
+    setTimeout(() => {
+        Array.from(document.getElementsByTagName('relative-time')).forEach(apply);
+        Array.from(document.getElementsByTagName('time-ago')).forEach(apply);
+    }, 1000);
 }
 
 function apply(node) {
     let date = new Date(node.getAttribute('datetime'));
-    let absDateTime = date.toLocaleString('ja-JP', {timeZone:'JST'});
-    node.innerHTML +=  '(' + absDateTime + 'JST)';
+    let absDateTime = date.toLocaleString('ja-JP', { timeZone: 'JST' });
+    node.innerHTML += '(' + absDateTime + 'JST)';
     node.shadowRoot.innerHTML += '(' + absDateTime + 'JST)';
 }
 
